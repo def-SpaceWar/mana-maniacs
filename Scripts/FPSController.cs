4f6dbd955086df12ecae9b678a77c42ae4e39164
def-SpaceWar <aryanahire9086360780@gmail.com>
2026-06-03 22:56:54 -0400

    implemented awsd

--- Scripts/FPSController.cs
+++ Scripts/FPSController.cs
@@ -18,9 +18,9 @@ public partial class FPSController : CharacterBody3D
 
 		if (!IsOnFloor()) velocity += GetGravity() * (float)delta;
 
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) velocity.Y = JumpVelocity;
+		if (Input.IsActionJustPressed("player_jump") && IsOnFloor()) velocity.Y = JumpVelocity;
 
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("player_left", "player_right", "player_forward", "player_backward");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
